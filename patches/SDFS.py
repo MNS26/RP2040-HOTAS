f429c392c48724f6a52e9013313367a7a78a835a
MNS26 <michaelsnip26@gmail.com>
2024-06-13 15:16:53 +0200

    Fix CI not building due to function name mismatch (fixed with .patch)

---
+++ patches/SDFS.py
@@ -0,0 +1,23 @@
+from os.path import join, isfile
+
+Import("env")
+
+FRAMEWORK_DIR = env.PioPlatform().get_package_dir("framework-arduinopico")
+patchflag_path = join(FRAMEWORK_DIR, ".patching-done")
+
+# patch file only if we didn't do it before
+if not isfile(join(FRAMEWORK_DIR, ".patching-done")):
+    original_file = join(FRAMEWORK_DIR, "libraries", "SDFS", "src", "SDFS.h")
+    patched_file = join("SDFS.patch")
+
+    assert isfile(original_file) and isfile(patched_file)
+
+    env.Execute("patch %s %s" % (original_file, patched_file))
+    # env.Execute("touch " + patchflag_path)
+
+
+    def _touch(path):
+        with open(path, "w") as fp:
+            fp.write("")
+
+    env.Execute(lambda *args, **kwargs: _touch(patchflag_path))
